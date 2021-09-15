f599b0a59af400e3d23f86995e2340bcedf4e830
Kevin Leitich <kevleitich@gmail.com>
2021-09-15 23:37:18 +0300

    Mandatory and advanced tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
-
-X = [[1,2,3],
-    [ ,5,6],
-    [7 ,8,9]]
-
-result = [[0,0,0],
-         [0,0,0],
-         [0,0,0]]
-
-# iterate through rows
-for i in range(len(X)):
-   # iterate through columns
-   for j in range(len(X[0])):
-       result[i][j] = X[i][j] * X[i][j]
-
-for r in result:
-   print(r)
+def square_matrix_simple(matrix=[]):
+    return [list(map((lambda x: x * x), elm)) for elm in matrix]
