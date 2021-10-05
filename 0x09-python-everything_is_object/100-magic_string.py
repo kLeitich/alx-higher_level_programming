4c3fa5767e97ac6dc35bdf49f8bff937106580db
kLeitich <kevleitich@gmail.com>
2021-10-06 00:23:16 +0300

    Mandatory and Advanced Tasks

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string(number=[0]):
-    number[0] = number[0] + 1
-    return "Holberton, " * (number[0] - 1) + "Holberton, "[:-2]
-
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-class LockedClass():
-    """Prevents the user from dynamically creating new instance attributes,
-    except if the new instance attribute is called first_name"""
-    __slots__ = ['first_name']
+# 101-locked_class.py
+"""Defines a locked class."""
 
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
