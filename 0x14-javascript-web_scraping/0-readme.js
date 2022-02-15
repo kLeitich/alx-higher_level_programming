500099d52de09622b74f1676619041c57f8dfac9
kLeitich <kevleitich@gmail.com>
2022-02-15 14:45:10 +0300

    fixing an error on readmemjs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
+// reads file passed as arg
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  console.log(error || data);
 });
